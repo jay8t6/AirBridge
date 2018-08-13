908df1a0305ee7b2c563aefd83729e5d17d5a660
Jaganathan Kaliyaperumal <Jagan.Kaliyaperumal@maritz.com>
2018-08-13 15:32:31 -0500

    Revert "Update: revert airbridge code"
    This reverts commit 196a48d3bf714ac6da6ef7a14f09611ebd3bb53d.

--- airbridge/airbridge.js
+++ airbridge/airbridge.js
@@ -1,30 +1,55 @@
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  if (/windows phone/i.test(userAgent)) {
+    return "Windows Phone";
+  }
+
+  if (/android/i.test(userAgent)) {
+    return "Android";
+  }
+
+  // iOS detection from: http://stackoverflow.com/a/9039885/177710
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    return "iOS";
+  }
+
+  return "unknown";
+}
+
+var sys = getMobileOperatingSystem();
+
+
 var AirBridge = (function() {
-	var instance;
+  var instance;
 
-	function createInstance() {
-		var object = new Object();
-		object.useWindowLocation = true;
-		return object;
-	}
+  function createInstance() {
+    var object = new Object();
+    object.useWindowLocation = true;
+    return object;
+  }
 
-	return {
-		setUseWindowLocation: function($shouldUseWindowLocation) {
-			if (!instance) {
-				instance = createInstance();
-			}
-			instance.useWindowLocation = $shouldUseWindowLocation;
-		},
+  return {
+    setUseWindowLocation: function($shouldUseWindowLocation) {
+      if (!instance) {
+        instance = createInstance();
+      }
+      instance.useWindowLocation = $shouldUseWindowLocation;
+    },
 
-		message: function($message) {
-			if (!instance) {
-				instance = createInstance();
-			}
-
-			if (!instance.useWindowLocation) {
-				NativeWebView.airBridge($message);
-			} else {
-				window.location = "http://airBridge:" + $message;
-			}
-		}
-	};
+    message: function($message) {
+      if (!instance) {
+        instance = createInstance();
+      }
+      if (!instance.useWindowLocation) {
+        NativeWebView.airBridge($message);
+      } else {
+        if (sys == "iOS") {
+          window.location = "airBridge:" + $message;
+        } else {
+          window.location = "http://airBridge:" + $message;
+        }
+      }
+    }
+  };
 })();
